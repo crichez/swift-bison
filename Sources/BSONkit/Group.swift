977b3625e97ed48e0c4d852ff07f7156cfc0e3b6
Christopher Richez <crichez@pm.me>
2022-03-01 20:47:01 -0500

    make `Group` public add documentation for `Gorup`

--- Sources/BSONkit/Group.swift
+++ Sources/BSONkit/Group.swift
@@ -1,20 +1,24 @@
 //
+//  Group.swift
+//  Group
 //
-//
-//
-//
+//  Created by Christopher Richez on March 1 2022
 //
 
-struct Group<T: DocComponent>: DocComponent {
-    typealias Encoded = T.Encoded
-
+/// A group of `DocComponent` values.
+/// 
+/// Use `Group` when you need to assign more than 10 key-value pairs in a document or
+/// `DocBuilder` declaration. Grouping document components does not affect the structure of the
+/// encoded document.
+public struct Group<T: DocComponent>: DocComponent {
     let content: T
 
-    init(@DocBuilder content: () -> T) {
-        self.content = content()
+    /// Groups the provided document components into a single document component.
+    public init(@DocBuilder content: () throws -> T) rethrows {
+        self.content = try content()
     }
 
-    var bsonEncoded: Encoded {
+    public var bsonEncoded: T.Encoded {
         content.bsonEncoded
     }
 }
