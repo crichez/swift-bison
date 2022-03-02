1abf67826d04a359a916656fd9363f983f329b25
Christopher Richez <crichez@pm.me>
2022-03-01 20:22:07 -0500

    renamed `Tuples.swift` to `Tuple.swift` updated `Tuple+DocComponent` templace updated `Package.swift` for new resources

--- Package.swift
+++ Package.swift
@@ -37,7 +37,7 @@ let package = Package(
             ],
             resources: [
                 .copy("Chains/Sequence.swifttemplate"),
-                .copy("Tuples/DocComponent.swifttemplate"),
+                .copy("Tuples/Tuple+DocComponent.swifttemplate"),
                 .copy("DocBuilderBlocks.swifttemplate"),
                 .copy("Chains/Collection.swifttemplate"),
             ]),
