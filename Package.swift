bae5f2c05f49342c5d0e7f133c1e1320e9a97087
Christopher Richez <crichez@pm.me>
2022-03-04 09:35:13 -0500

    reorganized project directories

--- Package.swift
+++ Package.swift
@@ -5,27 +5,13 @@ import PackageDescription
 
 let package = Package(
     name: "swift-bson-kit",
-    platforms: [
-        .macOS(.v10_15)
-    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "BSONKit",
             targets: ["BSONKit"]),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-        //.package(
-        //    name: "swift-collections",
-        //    url: "https://github.com/apple/swift-collections",
-        //    .upToNextMajor(from: "1.0.0")),
-        //.package(
-        //    name: "swift-algorithms",
-        //    url: "https://github.com/apple/swift-algorithms",
-        //    .upToNextMajor(from: "1.0.0")),
-    ],
+    dependencies: [],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
@@ -36,10 +22,10 @@ let package = Package(
                 //.product(name: "Algorithms", package: "swift-algorithms")
             ],
             resources: [
-                .copy("DocBuilder.swifttemplate"),
-                .copy("Tuples/Tuple+DocComponent.swifttemplate"),
-                .copy("Chains/Chain+Sequence.swifttemplate"),
-                .copy("Chains/Chain+Collection.swifttemplate"),
+                .copy("Encoding/DocBuilder.swifttemplate"),
+                .copy("Encoding/Tuples/Tuple+DocComponent.swifttemplate"),
+                .copy("Encoding/Chains/Chain+Sequence.swifttemplate"),
+                .copy("Encoding/Chains/Chain+Collection.swifttemplate"),
             ]),
         .testTarget(
             name: "BSONKitTests",
