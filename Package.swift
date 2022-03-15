a9b062d29004022bbc44df37e4dbfc9d70b384ae
Christopher Richez <crichez@pm.me>
2022-03-15 18:52:25 -0500

    Bumped swift-tools to 5.6

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.6
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -15,10 +15,7 @@ let package = Package(
             targets: ["BSONKit"]),
     ],
     dependencies: [
-        .package(
-            name: "swift-collections", 
-            url: "https://github.com/apple/swift-collections", 
-            .upToNextMajor(from: "1.0.0"))
+        .package(url: "https://github.com/apple/swift-collections", .upToNextMajor(from: "1.0.0"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -30,7 +27,6 @@ let package = Package(
             ],
             resources: [
                 .copy("Encoding/DocBuilder.swifttemplate"),
-                .copy("Encoding/Tuples/Tuple+DocComponent.swifttemplate"),
                 .copy("Encoding/Chains/Chain+Sequence.swifttemplate"),
                 .copy("Encoding/Chains/Chain+Collection.swifttemplate"),
             ]),
