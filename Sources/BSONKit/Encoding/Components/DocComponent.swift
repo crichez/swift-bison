736ff3f4f0a80c0fbeb68ce53479f6d8426a71ec
Christopher Richez <crichez@pm.me>
2022-03-16 15:26:06 -0400

    Simplified documentation for DocComponent

--- Sources/BSONKit/Encoding/Components/DocComponent.swift
+++ Sources/BSONKit/Encoding/Components/DocComponent.swift
@@ -5,10 +5,8 @@
 //  Created by Christopher Richez on March 1 2022
 //
 
-/// A value that can be converted into a collection of BSON-encoded bytes.
-/// 
-/// BSON document components include `Pair`, `Group` and `ForEach`.
+/// A basic building block of a BSON document.
 public protocol DocComponent {
-    /// This value's BSON-encoded bytes.
+    /// This component's BSON-encoded bytes.
     var bsonEncoded: [UInt8] { get }
 }
