cf2eeb4d82dd66f05282ec485f9ecfe8154b9d9a
Christopher Richez <crichez@pm.me>
2022-03-16 15:35:33 -0400

    Added documentation to `OptionalComponent`.

--- Sources/BSONKit/Encoding/Components/OptionalComponent.swift
+++ Sources/BSONKit/Encoding/Components/OptionalComponent.swift
@@ -1,14 +1,19 @@
 //
+//  OptionalComponent.swift
+//  OptionalComponent
 //
-//
-//
-//
+//  Created by Christopher Richez on March 16 2022
 //
 
+/// A wrapper type for an optional component used exclusively by ``DocBuilder/buildOptional(_:)``.
 enum OptionalComponent<T: DocComponent>: DocComponent {
+    /// A non-nil component.
     case some(T)
+    
+    /// A nil component.
     case none
-
+    
+    /// Initializes an `OptionalComponent` from the provided `Optional` document component.
     init(_ component: T?) {
         switch component {
         case .some(let component):
