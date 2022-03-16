b0a75fe13de15d778f8ee6a280239c25a6720749
Christopher Richez <crichez@pm.me>
2022-03-16 15:23:46 -0400

    Simplified ForEach internals

--- Sources/BSONKit/Encoding/Components/ForEach.swift
+++ Sources/BSONKit/Encoding/Components/ForEach.swift
@@ -5,64 +5,28 @@
 //  Created by Christopher Richez on March 1 2022
 //
 
-/// Encodes a `Sequence` of arbitrary type into a BSON document by applying the provided transform.
-public struct ForEach<Source: Sequence, Element: DocComponent> {
-    /// The sequence to which to apply the provided `transform` for each element.
-    let source: Source
+/// Encodes a sequence into a document using the provided closure.
+public struct ForEach<Base: Sequence, Element: DocComponent>: DocComponent {
+    /// The transformed elements of this loop.
+    let elements: LazyMapSequence<Base, Element>
 
-    /// A closure that takes a `source` element and returns a `DocComponent` conforming value.
-    /// 
-    /// This closure will be applied to each element in the `source` sequence 
-    /// when `encode()` is called.
-    let transform: (Source.Element) -> Element
-
-    /// Encodes a sequence of elements by providing a `DocComponent` value for each element.
-    /// 
-    /// - Parameters:
-    ///   - source: a `Sequence` of arbitrary type to which to apply a transformation
-    ///   - transform: a closure that takes a `source` element 
-    ///     and returns a `DocComponent` value.
+    /// Encodes a sequence of elements by returning a `DocComponent` for each element.
     ///
-    /// - Usage:
+    /// The following example encodes ten key value pairs from the `Int64` sequence 0 to 9.
     /// ```swift
-    /// ForEach(0..<10) { number in 
-    ///     String(describing: number) => number
+    /// ForEach(Int64(0)..<10) { number in
+    ///     "\(number)" => number
     /// }
     /// ```
-    public init(_ source: Source, @DocBuilder transform: @escaping (Source.Element) -> Element) {
-        self.source = source
-        self.transform = transform
+    /// 
+    /// - Parameters:
+    ///   - base: a `Sequence` of arbitrary type to which to return a document component
+    ///   - transform: a closure that takes a `base` element and returns a document component.
+    public init(_ base: Base, @DocBuilder transform: @escaping (Base.Element) -> Element) {
+        self.elements = base.lazy.map(transform)
     }
-}
-
-extension ForEach: Sequence {
-    /// An iterator that applies a transform to each `Source` element before returning it.
-    public struct Iterator: IteratorProtocol {
-        /// The iterator of the `Source` sequence.
-        var source: Source.Iterator
-        
-        /// The transforming closure to apply to each `source` element.
-        let transform: (Source.Element) -> Element
-
-        /// Initializes an iterator from the provided `ForEach` value.
-        init(_ loop: ForEach) {
-            self.source = loop.source.makeIterator()
-            self.transform = loop.transform
-        }
-
-        public mutating func next() -> Element? {
-            guard let nextSourceElement = source.next() else { return nil }
-            return transform(nextSourceElement)
-        }
-    }
-
-    public func makeIterator() -> Iterator {
-        Iterator(self)
-    }
-}
-
-extension ForEach: DocComponent {
+    
     public var bsonEncoded: [UInt8] {
-        flatMap { $0.bsonEncoded }
+        elements.flatMap { $0.bsonEncoded }
     }
 }
