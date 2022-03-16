8fda2f84db954ad89ece29535df787560d1d9e3a
Christopher Richez <crichez@pm.me>
2022-03-16 15:49:36 -0400

    Made `TupleX` types internal. Moved `Tuple10` extension to end of file.

--- Sources/BSONKit/Encoding/Tuples/Tuple+DocComponent.swift
+++ Sources/BSONKit/Encoding/Tuples/Tuple+DocComponent.swift
@@ -5,70 +5,28 @@
 //  Created by Christopher Richez on March 15 2022
 //
 
-extension Tuple10: DocComponent where T0: DocComponent, T1: DocComponent, T2: DocComponent, 
-  T3: DocComponent, T4: DocComponent, T5: DocComponent, T6: DocComponent, T7: DocComponent, 
-  T8: DocComponent, T9: DocComponent {
-    public var bsonEncoded: [UInt8] {
-        var concatenatedComponents = t0.bsonEncoded
-        concatenatedComponents.append(contentsOf: t1.bsonEncoded)
-        concatenatedComponents.append(contentsOf: t2.bsonEncoded)
-        concatenatedComponents.append(contentsOf: t3.bsonEncoded)
-        concatenatedComponents.append(contentsOf: t4.bsonEncoded)
-        concatenatedComponents.append(contentsOf: t5.bsonEncoded)
-        concatenatedComponents.append(contentsOf: t6.bsonEncoded)
-        concatenatedComponents.append(contentsOf: t7.bsonEncoded)
-        concatenatedComponents.append(contentsOf: t8.bsonEncoded)
-        concatenatedComponents.append(contentsOf: t9.bsonEncoded)
-        return concatenatedComponents
-    }
-
-    public init(
-        _ t0: T0, 
-        _ t1: T1, 
-        _ t2: T2, 
-        _ t3: T3, 
-        _ t4: T4, 
-        _ t5: T5, 
-        _ t6: T6, 
-        _ t7: T7, 
-        _ t8: T8, 
-        _ t9: T9
-    ) {
-        self.t0 = t0
-        self.t1 = t1
-        self.t2 = t2
-        self.t3 = t3
-        self.t4 = t4
-        self.t5 = t5
-        self.t6 = t6
-        self.t7 = t7
-        self.t8 = t8
-        self.t9 = t9
-    }
-}
-
 extension Tuple2: DocComponent where T0: DocComponent, T1: DocComponent {
-    public var bsonEncoded: [UInt8] {
+    var bsonEncoded: [UInt8] {
         var concatenatedComponents = t0.bsonEncoded
         concatenatedComponents.append(contentsOf: t1.bsonEncoded)
         return concatenatedComponents
     }
 
-    public init(_ t0: T0,_ t1: T1) {
+    init(_ t0: T0,_ t1: T1) {
         self.t0 = t0
         self.t1 = t1
     }
 }
 
 extension Tuple3: DocComponent where T0: DocComponent, T1: DocComponent, T2: DocComponent {
-    public var bsonEncoded: [UInt8] {
+    var bsonEncoded: [UInt8] {
         var concatenatedComponents = t0.bsonEncoded
         concatenatedComponents.append(contentsOf: t1.bsonEncoded)
         concatenatedComponents.append(contentsOf: t2.bsonEncoded)
         return concatenatedComponents
     }
 
-    public init(_ t0: T0,_ t1: T1,_ t2: T2) {
+    init(_ t0: T0,_ t1: T1,_ t2: T2) {
         self.t0 = t0
         self.t1 = t1
         self.t2 = t2
@@ -77,7 +35,7 @@ extension Tuple3: DocComponent where T0: DocComponent, T1: DocComponent, T2: Doc
 
 extension Tuple4: DocComponent where T0: DocComponent, T1: DocComponent, T2: DocComponent, 
   T3: DocComponent {
-    public var bsonEncoded: [UInt8] {
+    var bsonEncoded: [UInt8] {
         var concatenatedComponents = t0.bsonEncoded
         concatenatedComponents.append(contentsOf: t1.bsonEncoded)
         concatenatedComponents.append(contentsOf: t2.bsonEncoded)
@@ -85,7 +43,7 @@ extension Tuple4: DocComponent where T0: DocComponent, T1: DocComponent, T2: Doc
         return concatenatedComponents
     }
 
-    public init(_ t0: T0,_ t1: T1,_ t2: T2,_ t3: T3) {
+    init(_ t0: T0,_ t1: T1,_ t2: T2,_ t3: T3) {
         self.t0 = t0
         self.t1 = t1
         self.t2 = t2
@@ -95,7 +53,7 @@ extension Tuple4: DocComponent where T0: DocComponent, T1: DocComponent, T2: Doc
 
 extension Tuple5: DocComponent where T0: DocComponent, T1: DocComponent, T2: DocComponent, 
   T3: DocComponent, T4: DocComponent {
-    public var bsonEncoded: [UInt8] {
+    var bsonEncoded: [UInt8] {
         var concatenatedComponents = t0.bsonEncoded
         concatenatedComponents.append(contentsOf: t1.bsonEncoded)
         concatenatedComponents.append(contentsOf: t2.bsonEncoded)
@@ -104,7 +62,7 @@ extension Tuple5: DocComponent where T0: DocComponent, T1: DocComponent, T2: Doc
         return concatenatedComponents
     }
 
-    public init(_ t0: T0,_ t1: T1,_ t2: T2,_ t3: T3,_ t4: T4) {
+    init(_ t0: T0,_ t1: T1,_ t2: T2,_ t3: T3,_ t4: T4) {
         self.t0 = t0
         self.t1 = t1
         self.t2 = t2
@@ -115,7 +73,7 @@ extension Tuple5: DocComponent where T0: DocComponent, T1: DocComponent, T2: Doc
 
 extension Tuple6: DocComponent where T0: DocComponent, T1: DocComponent, T2: DocComponent, 
   T3: DocComponent, T4: DocComponent, T5: DocComponent {
-    public var bsonEncoded: [UInt8] {
+    var bsonEncoded: [UInt8] {
         var concatenatedComponents = t0.bsonEncoded
         concatenatedComponents.append(contentsOf: t1.bsonEncoded)
         concatenatedComponents.append(contentsOf: t2.bsonEncoded)
@@ -125,7 +83,7 @@ extension Tuple6: DocComponent where T0: DocComponent, T1: DocComponent, T2: Doc
         return concatenatedComponents
     }
 
-    public init(_ t0: T0,_ t1: T1,_ t2: T2,_ t3: T3,_ t4: T4,_ t5: T5) {
+    init(_ t0: T0,_ t1: T1,_ t2: T2,_ t3: T3,_ t4: T4,_ t5: T5) {
         self.t0 = t0
         self.t1 = t1
         self.t2 = t2
@@ -137,7 +95,7 @@ extension Tuple6: DocComponent where T0: DocComponent, T1: DocComponent, T2: Doc
 
 extension Tuple7: DocComponent where T0: DocComponent, T1: DocComponent, T2: DocComponent, 
   T3: DocComponent, T4: DocComponent, T5: DocComponent, T6: DocComponent {
-    public var bsonEncoded: [UInt8] {
+    var bsonEncoded: [UInt8] {
         var concatenatedComponents = t0.bsonEncoded
         concatenatedComponents.append(contentsOf: t1.bsonEncoded)
         concatenatedComponents.append(contentsOf: t2.bsonEncoded)
@@ -148,7 +106,7 @@ extension Tuple7: DocComponent where T0: DocComponent, T1: DocComponent, T2: Doc
         return concatenatedComponents
     }
 
-    public init(_ t0: T0,_ t1: T1,_ t2: T2,_ t3: T3,_ t4: T4,_ t5: T5,_ t6: T6) {
+    init(_ t0: T0,_ t1: T1,_ t2: T2,_ t3: T3,_ t4: T4,_ t5: T5,_ t6: T6) {
         self.t0 = t0
         self.t1 = t1
         self.t2 = t2
@@ -161,7 +119,7 @@ extension Tuple7: DocComponent where T0: DocComponent, T1: DocComponent, T2: Doc
 
 extension Tuple8: DocComponent where T0: DocComponent, T1: DocComponent, T2: DocComponent, 
   T3: DocComponent, T4: DocComponent, T5: DocComponent, T6: DocComponent, T7: DocComponent {
-    public var bsonEncoded: [UInt8] {
+    var bsonEncoded: [UInt8] {
         var concatenatedComponents = t0.bsonEncoded
         concatenatedComponents.append(contentsOf: t1.bsonEncoded)
         concatenatedComponents.append(contentsOf: t2.bsonEncoded)
@@ -173,7 +131,7 @@ extension Tuple8: DocComponent where T0: DocComponent, T1: DocComponent, T2: Doc
         return concatenatedComponents
     }
 
-    public init(_ t0: T0,_ t1: T1,_ t2: T2,_ t3: T3,_ t4: T4,_ t5: T5,_ t6: T6,_ t7: T7) {
+    init(_ t0: T0,_ t1: T1,_ t2: T2,_ t3: T3,_ t4: T4,_ t5: T5,_ t6: T6,_ t7: T7) {
         self.t0 = t0
         self.t1 = t1
         self.t2 = t2
@@ -188,7 +146,7 @@ extension Tuple8: DocComponent where T0: DocComponent, T1: DocComponent, T2: Doc
 extension Tuple9: DocComponent where T0: DocComponent, T1: DocComponent, T2: DocComponent, 
   T3: DocComponent, T4: DocComponent, T5: DocComponent, T6: DocComponent, T7: DocComponent, 
   T8: DocComponent {
-    public var bsonEncoded: [UInt8] {
+    var bsonEncoded: [UInt8] {
         var concatenatedComponents = t0.bsonEncoded
         concatenatedComponents.append(contentsOf: t1.bsonEncoded)
         concatenatedComponents.append(contentsOf: t2.bsonEncoded)
@@ -201,7 +159,7 @@ extension Tuple9: DocComponent where T0: DocComponent, T1: DocComponent, T2: Doc
         return concatenatedComponents
     }
 
-    public init(_ t0: T0,_ t1: T1,_ t2: T2,_ t3: T3,_ t4: T4,_ t5: T5,_ t6: T6,_ t7: T7,_ t8: T8) {
+    init(_ t0: T0,_ t1: T1,_ t2: T2,_ t3: T3,_ t4: T4,_ t5: T5,_ t6: T6,_ t7: T7,_ t8: T8) {
         self.t0 = t0
         self.t1 = t1
         self.t2 = t2
@@ -214,3 +172,44 @@ extension Tuple9: DocComponent where T0: DocComponent, T1: DocComponent, T2: Doc
     }
 }
 
+extension Tuple10: DocComponent where T0: DocComponent, T1: DocComponent, T2: DocComponent,
+  T3: DocComponent, T4: DocComponent, T5: DocComponent, T6: DocComponent, T7: DocComponent,
+  T8: DocComponent, T9: DocComponent {
+    var bsonEncoded: [UInt8] {
+        var concatenatedComponents = t0.bsonEncoded
+        concatenatedComponents.append(contentsOf: t1.bsonEncoded)
+        concatenatedComponents.append(contentsOf: t2.bsonEncoded)
+        concatenatedComponents.append(contentsOf: t3.bsonEncoded)
+        concatenatedComponents.append(contentsOf: t4.bsonEncoded)
+        concatenatedComponents.append(contentsOf: t5.bsonEncoded)
+        concatenatedComponents.append(contentsOf: t6.bsonEncoded)
+        concatenatedComponents.append(contentsOf: t7.bsonEncoded)
+        concatenatedComponents.append(contentsOf: t8.bsonEncoded)
+        concatenatedComponents.append(contentsOf: t9.bsonEncoded)
+        return concatenatedComponents
+    }
+
+    init(
+        _ t0: T0,
+        _ t1: T1,
+        _ t2: T2,
+        _ t3: T3,
+        _ t4: T4,
+        _ t5: T5,
+        _ t6: T6,
+        _ t7: T7,
+        _ t8: T8,
+        _ t9: T9
+    ) {
+        self.t0 = t0
+        self.t1 = t1
+        self.t2 = t2
+        self.t3 = t3
+        self.t4 = t4
+        self.t5 = t5
+        self.t6 = t6
+        self.t7 = t7
+        self.t8 = t8
+        self.t9 = t9
+    }
+}
--- Sources/BSONKit/Encoding/Tuples/Tuple.swift
+++ Sources/BSONKit/Encoding/Tuples/Tuple.swift
@@ -5,25 +5,25 @@
 //  Created by Christopher Richez on February 11 2022
 //
 
-public struct Tuple2<T0, T1> {
+struct Tuple2<T0, T1> {
     let t0: T0
     let t1: T1
 }
 
-public struct Tuple3<T0, T1, T2> {
+struct Tuple3<T0, T1, T2> {
     let t0: T0
     let t1: T1
     let t2: T2
 }
 
-public struct Tuple4<T0, T1, T2, T3> {
+struct Tuple4<T0, T1, T2, T3> {
     let t0: T0
     let t1: T1
     let t2: T2
     let t3: T3
 }
 
-public struct Tuple5<T0, T1, T2, T3, T4> {
+struct Tuple5<T0, T1, T2, T3, T4> {
     let t0: T0
     let t1: T1
     let t2: T2
@@ -31,7 +31,7 @@ public struct Tuple5<T0, T1, T2, T3, T4> {
     let t4: T4
 }
 
-public struct Tuple6<T0, T1, T2, T3, T4, T5> {
+struct Tuple6<T0, T1, T2, T3, T4, T5> {
     let t0: T0
     let t1: T1
     let t2: T2
@@ -40,7 +40,7 @@ public struct Tuple6<T0, T1, T2, T3, T4, T5> {
     let t5: T5
 }
 
-public struct Tuple7<T0, T1, T2, T3, T4, T5, T6> {
+struct Tuple7<T0, T1, T2, T3, T4, T5, T6> {
     let t0: T0
     let t1: T1
     let t2: T2
@@ -50,7 +50,7 @@ public struct Tuple7<T0, T1, T2, T3, T4, T5, T6> {
     let t6: T6
 }
 
-public struct Tuple8<T0, T1, T2, T3, T4, T5, T6, T7> {
+struct Tuple8<T0, T1, T2, T3, T4, T5, T6, T7> {
     let t0: T0
     let t1: T1
     let t2: T2
@@ -61,7 +61,7 @@ public struct Tuple8<T0, T1, T2, T3, T4, T5, T6, T7> {
     let t7: T7
 }
 
-public struct Tuple9<T0, T1, T2, T3, T4, T5, T6, T7, T8> {
+struct Tuple9<T0, T1, T2, T3, T4, T5, T6, T7, T8> {
     let t0: T0
     let t1: T1
     let t2: T2
@@ -73,7 +73,7 @@ public struct Tuple9<T0, T1, T2, T3, T4, T5, T6, T7, T8> {
     let t8: T8
 }
 
-public struct Tuple10<T0, T1, T2, T3, T4, T5, T6, T7, T8, T9> {
+struct Tuple10<T0, T1, T2, T3, T4, T5, T6, T7, T8, T9> {
     let t0: T0
     let t1: T1
     let t2: T2
