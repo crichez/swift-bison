2ac73f84e14552c20e5040695d655deb592e84b2
Christopher Richez <crichez@pm.me>
2022-03-17 11:28:52 -0500

    Added the `DocComponentTests` case.

---
+++ Tests/BSONKitTests/DocComponentTests.swift
@@ -0,0 +1,14 @@
+//
+//  DocComponentTests.swift
+//  DocComponentTests
+//
+//  Created by Christopher Richez on March 17 2022
+//
+
+import BSONKit
+import XCTest
+
+/// A test suite for `DocComponent` conforming types.
+class DocComponentTests: XCTestCase {
+
+}
