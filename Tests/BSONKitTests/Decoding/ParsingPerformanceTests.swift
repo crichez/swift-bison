11b86a5cd65ebed2f9a2209bf1920996256faf83
Christopher Richez <crichez@pm.me>
2022-03-18 23:28:32 -0400

    Added a test case and method for parsing performance.

---
+++ Tests/BSONKitTests/Decoding/ParsingPerformanceTests.swift
@@ -0,0 +1,28 @@
+//
+//  ParsingPerformanceTests.swift
+//  ParsingPerformanceTests
+//
+//  Created by Christopher Richez on March 18 2022
+//
+
+import BSONKit
+import XCTest
+
+class ParsingPerformanceTests: XCTestCase {
+    func testLargeDoc() throws {
+        let doc = ComposedDocument {
+            ForEach(Int64(0)..<1000) { n in 
+                "\(n)" => n
+            }
+        }
+        let encodedDoc = doc.bsonBytes
+        let options = XCTMeasureOptions()
+        options.iterationCount = 100
+        measure(options: options) {
+            let decodedDoc = try! BSONParser(bsonData: encodedDoc).parse()
+            for n in Int64(0)..<1000 {
+                let _ = try! Int64(bsonData: decodedDoc["\(n)"]!)
+            }
+        }
+    }
+}
