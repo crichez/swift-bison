3a1489b104f9088d3a5d7d4b915b37f661ba4bdf
Christopher Richez <crichez@pm.me>
2022-03-18 22:43:14 -0400

    Added the EncodingPerformanceTests case.

---
+++ Tests/BSONKitTests/Encoding/EncodingPerformanceTests.swift
@@ -0,0 +1,13 @@
+//
+//  EncodingPerformanceTests.swift
+//  EncodingPerformanceTests
+//
+//  Created by Christopher Richez on March 18 2022
+//
+
+import XCTest
+import BSONKit
+
+class EncodingPerformanceTests: XCTestCase {
+    
+}
