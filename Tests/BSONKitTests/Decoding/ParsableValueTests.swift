f94fbdfaada501539b186214a65cb4bf9deec056
Christopher Richez <crichez@pm.me>
2022-03-20 21:53:04 -0400

    Added testDoubleSizeMismatch.

--- Tests/BSONKitTests/Decoding/ParsableValueTests.swift
+++ Tests/BSONKitTests/Decoding/ParsableValueTests.swift
@@ -16,4 +16,16 @@ class ParsableValueTests: XCTestCase {
         let decodedValue = try Double(bsonBytes: value.bsonBytes)
         XCTAssertEqual(value, decodedValue)
     }
+
+    /// Asserts attempting to decode a `Double` from other than 8 bytes throws
+    /// `Double.Error.sizeMismatch`.
+    func testDoubleSizeMismatch() throws {
+        do {
+            let faultyBytes: [UInt8] = [0, 1, 2, 3]
+            let decodedValue = try Double(bsonBytes: faultyBytes)
+            XCTFail("expected decoding to fail, but returned \(decodedValue)")
+        } catch Double.Error.sizeMismatch {
+            return
+        }
+    }
 }
