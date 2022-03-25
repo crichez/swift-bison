2b54bbafedcd120308c8453677528b2c2ad8e6a9
Christopher Richez <crichez@pm.me>
2022-03-25 09:55:40 -0400

    Added testFixedValueSizeMismatch().

--- Tests/BSONKitTests/Decoding/ParsedDocumentTests.swift
+++ Tests/BSONKitTests/Decoding/ParsedDocumentTests.swift
@@ -11,6 +11,8 @@ import XCTest
 
 /// Tests internal functionality of the `ParsedDocument` type.
 class ParsedDocumentTests: XCTestCase {
+    let typeMap = ParsedDocument<[UInt8]>.typeMap
+
     /// Asserts encoding and decoding a valid document returns the same value.
     func testDocParsed() throws {
         let doc = ComposedDocument {
@@ -81,6 +83,25 @@ class ParsedDocumentTests: XCTestCase {
                 parsed: partialDoc, 
                 remaining: faultyBytes[7...])
             XCTAssertEqual(error, .unknownType(100, "", progress))
+        }
+    }
+
+    /// Asserts parsing a document with a value truncated to less than its expected number of bytes
+    /// throws `ParsedDocument<_>.Error.valueSizeMismatch` with the expected attached values.
+    /// 
+    /// This test uses `Double` as its fixed-size type of choice. The logic is the same for other
+    /// fixed-size types, so this test satisfies those requirements as well.
+    func testFixedValueSizeMismatch() throws {
+        let faultyBytes = [10, 0, 0, 0, 1, 0] + [UInt8](repeating: 1, count: 3) + [0]
+        do {
+            let decodedDoc = try ParsedDocument(bsonBytes: faultyBytes)
+            XCTFail("expected decoding to fail, but returned \(decodedDoc)")
+        } catch let error as ParsedDocument<[UInt8]>.Error {
+            let partialDoc = ParsedDocument<[UInt8]>([:])
+            let progress = ParsedDocument<[UInt8]>.Progress(
+                parsed: partialDoc, 
+                remaining: faultyBytes[6...])
+            XCTAssertEqual(error, .valueSizeMismatch(8, "", progress))
         }
     }
 }
