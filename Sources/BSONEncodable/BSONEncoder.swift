6c47230307bf161f80de0b92758b3dcbfb4272e3
Christopher Richez <crichez@pm.me>
2022-04-03 14:17:18 -0400

    Notes lack of custom `ValueProtocol` support.

--- Sources/BSONEncodable/BSONEncoder.swift
+++ Sources/BSONEncodable/BSONEncoder.swift
@@ -6,11 +6,21 @@
 //
 
 /// Use `BSONEncoder` to encode `Encodable` values into fully-formed BSON documents.
+/// 
+/// - Note: 
+/// This encoder does not respect custom `ValueProtocol` implementations.
+/// To encode custom BSON values differently than their `Encodable` implementation,
+/// Use `ComposedDocument` or `ComposedArrayDocument`.
 public struct BSONEncoder {
     /// Initializes an encoder.
     public init() {}
 
     /// Encodes the provided value into a BSON document.
+    /// 
+    /// - Note: 
+    /// This encoder does not respect custom ``ValueProtocol`` implementations.
+    /// To encode custom BSON values differently than their `Encodable` implementation,
+    /// Use ``ComposedDocument`` or ``ComposedArrayDocument``.
     /// 
     /// - Throws: 
     /// Re-throws any errors thrown in the value's `encode(to:)` method.
